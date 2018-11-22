178ac2954cc59d9dac1c27e4426b9ca2a828ef1d
Mr Mjir <39204944+MrMjir@users.noreply.github.com>
2018-11-22 14:03:03 -0600

    Did big tings
    pak pak

--- docs/js/js.js
+++ docs/js/js.js
@@ -1,12 +1,5 @@
+//Main
 main();
-//Wait for entire page to load
-$(window).bind("load", function() {
-	setParallax();
-});
-//Functions on window resize
-window.onresize = function(e) {
-	setParallax();
-};
 
 function main() {
 	loadStuff();
@@ -15,19 +8,6 @@ function main() {
 function loadStuff() {
 	//Navigation
 	$("#top-nav").load("/docs/pages/top-nav.html");
-}
-
-function setParallax() {
-	for(var i = 1; i < $(".parallax-group").length; i++) {
-		var marginHeight = 0;
-		for (var j = 0; j < i; j++) {
-			for (var k = 0; k < $(".parallax-group:nth-child(" + (j + 1) + ") .section").length; k++) {
-				marginHeight += $(".parallax-group:nth-child(" + (j + 1) + ") .section:nth-child(" + (k + 1) + ")").outerHeight();
-				console.log( $(".parallax-group:nth-child(" + (j + 1) + ") .section:nth-child(" + (k + 1) + ")"));
-				console.log($(".parallax-group:nth-child(" + (j + 1) + ") .section:nth-child(" + (k + 1) + ")").outerHeight());
-			}
-		}
-		console.log(marginHeight);
-		$(".parallax-group:nth-child(" + (i + 1) + ")").css("margin-top", (marginHeight +  i * 1000) + "px");
-	}
+	//Footer
+	$("#footer").load("/docs/pages/footer.html");
 }
