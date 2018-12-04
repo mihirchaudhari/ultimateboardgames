cf634bb35d1e106843529f85edfb408941c66ea6
msc2913 <msc2913@g.coppellisd.com>
2018-12-04 11:37:20 -0600

    Update secret.js

--- docs/js/secret.js
+++ docs/js/secret.js
@@ -11,6 +11,6 @@ var clicked = 0;
 function imgClicked() {
 	clicked++;
 	if (clicked == 3) {
-		window.open("https://drive.google.com/file/d/1IWo7Tdfa-_mItVqjt7pdPUoN5qBp7Ff2/view?usp=sharing", "_blank");
+		window.open("https://drive.google.com/a/g.coppellisd.com/file/d/11qYiVBTFylLbeBKy3bPY5MlcY8ymma4v/view?usp=drivesdk", "_blank");
 	}
 }
